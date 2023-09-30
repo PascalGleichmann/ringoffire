62213e6c0aaa95ec11574d1f12193a680b1362d4
PascalGajewski <121040841+PascalGajewski@users.noreply.github.com>
2023-09-30 16:27:49 +0200

    fixed database

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,60 +1,60 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { Game } from '../models/game';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 import { MatDialog } from '@angular/material/dialog';
 import { FirebaseApp } from '@angular/fire/app';
-import { Firestore, collectionData, collection, onSnapshot } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, onSnapshot, doc, addDoc, docData, CollectionReference, DocumentReference } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
 
-  firestore: Firestore = inject(Firestore);
+export class GameComponent {
 
-  pickCardAnimation = false;
-  currentCard: string = '';
-  game!: Game;
+  game: Game = new Game();
+  gameDocId!: string;
+  collectionInstance!: CollectionReference;
+  documentInstance!: DocumentReference;
 
-  unsubGame: any;
-
-  constructor(public dialog: MatDialog) { }
-
-  ngOnInit() {
-    this.newGame();
-
-    this.unsubGame = onSnapshot(collection(this.firestore, "games"), (doc) => {
-      console.log("Current data: ", doc);
-    })
+  constructor(public dialog: MatDialog, private firestore: Firestore, private route: ActivatedRoute) {
+    this.collectionInstance = collection(this.firestore, 'games');
+    this.route.params.subscribe((params) => {
+      this.documentInstance = params['id'];
+      this.getGame(params['id']);
+    });
   }
 
-  ngOnDestroy(){
-    this.unsubGame();
-  }
-
-  newGame() {
-    this.game = new Game();
-    console.log(this.game);
+  getGame(docId: string) {
+    onSnapshot(doc(this.collectionInstance, docId), (game) => {
+      console.log(game.data());
+        this.game.players = game.data()!['players'];
+        this.game.stack = game.data()!['stack'];
+        this.game.playedCards = game.data()!['playedCards'];
+        this.game.currentPlayer = game.data()!['currentPlayer'];
+        this.game.pickCardAnimation = game.data()!['pickCardAnimation'];
+        this.game.currentCard = game.data()!['currentCard'];
+    });
   }
 
   takeCard() {
-    if (!this.pickCardAnimation) {
-      this.currentCard = this.game.stack.pop() ?? 'card_empty';
-      this.pickCardAnimation = true;
+    if (!this.game.pickCardAnimation) {
+      this.game.currentCard = this.game.stack.pop() ?? 'card_empty';
+      this.game.pickCardAnimation = true;
       this.game.currentPlayer++;
       this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
 
 
-      console.log('New Card: ' + this.currentCard);
+      console.log('New Card: ' + this.game.currentCard);
       console.log('Game is', this.game);
 
 
       setTimeout(() => {
-        this.game.playedCards.push(this.currentCard);
-        this.pickCardAnimation = false;
+        this.game.playedCards.push(this.game.currentCard);
+        this.game.pickCardAnimation = false;
       }, 1000);
     }
   }
--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Firestore, addDoc, collection } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { Game } from '../models/game';
 
 @Component({
   selector: 'app-start-screen',
@@ -7,11 +9,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./start-screen.component.scss']
 })
 export class StartScreenComponent {
+  collectionInstance = collection(this.firestore, 'games');
 
-  constructor(private router: Router) { };
+  constructor(private router: Router, private firestore: Firestore) { };
 
   // function starts a new game
   newGame() {
-    this.router.navigateByUrl('/game');
+    let game = new Game();
+    this.addData(game.toJson());
+  }
+
+  addData(game: any) {
+    if (game) {
+      addDoc(this.collectionInstance, game)
+        .then((gameInfo: any) => { this.router.navigateByUrl('/game/' + gameInfo.id)})
+        .catch((error) => {
+          console.error('Document could not saved: ', error);
+        })
+    }
   }
 }
